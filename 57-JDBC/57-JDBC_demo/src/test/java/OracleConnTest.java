ad0c32ac83b0c2efdcc01556ec4df0617d04adb3
sxudong <15295901@qq.com>
2024-02-06 23:10:39 +0800

    oracle conn read timeout test

--- 57-JDBC/57-JDBC_demo/src/test/java/OracleConnTest.java
+++ 57-JDBC/57-JDBC_demo/src/test/java/OracleConnTest.java
@@ -1,15 +1,27 @@
+import oracle.jdbc.pool.OracleDataSource;
 import org.junit.Test;
 import testOracleConn.DBUtil;
 
 import java.sql.*;
+import java.util.Properties;
 
 public class OracleConnTest {
     @Test
     public void testReadTimeout() throws Exception {
-        String readTimeout = "0"; // ms
-        System.setProperty("oracle.jdbc.ReadTimeout", readTimeout);
-        Class.forName("oracle.jdbc.OracleDriver");
-        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.113:1521:orcl", "scott", "tiger");
+        Properties prop = new Properties();
+        prop.setProperty("user" , "scott");
+        prop.setProperty("password" , "tiger");
+        prop.setProperty("oracle.net.CONNECT_TIMEOUT" , "10000000");
+        prop.setProperty("oracle.jdbc.ReadTimeout" , "1" );
+
+        OracleDataSource ods = new OracleDataSource();
+        ods.setConnectionProperties(prop);
+        ods.setURL("jdbc:oracle:thin:@192.168.56.113:1521:orcl");
+        Connection conn = ods.getConnection();
+
+//        System.setProperty("oracle.jdbc.ReadTimeout", "1");
+//        Class.forName("oracle.jdbc.OracleDriver");
+//        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.113:1521:orcl", "scott", "tiger");
 
         String sql = "SELECT * FROM student";
         PreparedStatement pstmt;
